3fbbc729923fc65326541567de3c3f65845b4ae3
Ivan-Matinez <ivan.martinez@bq.com>
2016-07-08 09:43:16 +0200

    First commit. Added all needed files. gradlew install working.

---
+++ src/main/groovy/com/bq/gradle/PoEditorPlugin.groovy
@@ -0,0 +1,22 @@
+package com.bq.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * PoEditor gradle plugin.
+ *
+ * Created by imartinez on 11/1/16.
+ */
+class PoEditorPlugin implements Plugin<Project> {
+
+    void apply(Project project) {
+
+        // Add the 'poEditorPlugin' extension object, used to pass parameters to the task
+        project.extensions.create("poEditorPlugin", PoEditorPluginExtension)
+
+        // Registers the task
+        project.task('importPoEditorStrings', type: ImportPoEditorStringsTask)
+    }
+
+}
---
+++ src/test/groovy/org/gradle/PoEditorPluginTest.groovy
@@ -0,0 +1,23 @@
+package org.gradle
+
+import com.bq.gradle.ImportPoEditorStringsTask
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Test
+
+import static org.junit.Assert.assertTrue
+
+/**
+ * PoEditorPluginTest test.
+ *
+ * Created by imartinez on 11/1/16.
+ */
+class PoEditorPluginTest {
+    @Test
+    public void poeditorPluginAddsImportPoEditorStringsTaskToProject() {
+        Project project = ProjectBuilder.builder().build()
+        project.apply plugin: 'com.bq.poeditor'
+
+        assertTrue(project.tasks.importPoEditorStrings instanceof ImportPoEditorStringsTask)
+    }
+}
